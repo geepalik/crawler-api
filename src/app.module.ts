cf16b724462b9d1dc170b20e1cf401afbf48d04d
gilpal <gil.palikaras@gmail.com>
2023-04-06 21:34:16 +0300

    basic configuration and db

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config/dist';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL')
+      }),
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
