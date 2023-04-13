c5c8847fe19706a202e8fe593b421402ed0025d5
gilpal <gil.palikaras@gmail.com>
2023-04-13 12:39:10 +0300

    rembe host from config, add .env.example file and readme

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 export default () => ({
   port: parseInt(process.env.PORT) || 3000,
-  host: process.env.HOST,
   databaseURL: process.env.DATABASE_URL,
   databaseContainerURL: process.env.DATABASE_CONTAINER_URL,
   localFilesPath: process.env.LOCAL_FILES_PATH,
